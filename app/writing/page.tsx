50c2e817cae402266269ecb17fade48d91c81efb
KennethRipley <kenneth@ripley.media>
2026-01-26 18:31:21 -0500

    Create page.tsx

---
+++ app/writing/page.tsx
@@ -0,0 +1,8 @@
+export default function WritingPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Writing</h1>
+      <p>If you can read this, /writing works.</p>
+    </main>
+  );
+}
